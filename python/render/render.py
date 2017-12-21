5d19cb263515ef0fb1b02427a7d22fe4acb33077
PlenopticToolbox <34269091+PlenopticToolbox@users.noreply.github.com>
2017-12-21 17:08:14 +0100

    Add files via upload

---
+++ python/render/render.py
@@ -0,0 +1,154 @@
+
+import numpy as np
+
+def render_lens_imgs(lenses, lens_imgs, img_shape=None):
+
+    """
+    Parameters
+    ----------
+
+    lenses: dictionary, keys are integer pairs (axial hex coordinates)
+            The lens dictionary
+
+    lens_imgs: dictionary
+               Dictionary with the lens data, same size as lenses
+
+    img_shape: pair of integers
+               Shape of the target image
+
+    Returns
+    -------
+
+    img:    array like
+            Two-dimensional array containing the microlens depth image
+            
+    """
+
+    assert len(lenses) == len(lens_imgs), "Number of lenses do not coincide"
+    assert len(lenses) > 0, "0 lenses supplied"
+    
+    first_lens = lenses[0, 0]
+
+    # ensure that the center lens is at the image origin
+    if img_shape is None:
+        img_shape = ((first_lens.pcoord) * 2 + 1).astype(int)
+
+    if len(first_lens.col_img.shape) == 3:
+        hl, wl, c = first_lens.col_img.shape
+    else:
+        hl, wl = first_lens.col_img.shape
+        c = 1
+
+    # here we create the structure for the image (circle images with the mask)
+    assert hl == wl
+    n = (hl - 1) / 2.0
+    x = np.linspace(-n, n, hl)
+
+    XX, YY = np.meshgrid(x, x)
+    ind = np.where(XX**2 + YY**2 < first_lens.inner_radius**2)
+    
+    # micro image, so it takes the shape of first_lens.col_img_shape
+    if len(first_lens.col_img.shape) == 3:
+        img = np.zeros((img_shape[0], img_shape[1], c))
+    else:
+        img = np.zeros((img_shape))
+    
+    for key in lenses:
+        
+        data = np.asarray(lens_imgs[key])
+
+        lens = lenses[key]
+        ty = (YY + lens.pcoord[0] + 0.5).astype(int)
+        tx = (XX + lens.pcoord[1] + 0.5).astype(int)
+        
+        # ensure that the subimg is located within the image bounds
+        if np.any(ty < 0) or np.any(tx < 0) or np.any(ty >= img_shape[0]) or np.any(tx >= img_shape[1]):
+            continue
+
+        if len(data.shape) > 0:
+            img[(ty[ind], tx[ind])] = data[ind]
+        else:
+            img[(ty[ind], tx[ind])] = data
+    
+
+    
+    return img
+    
+def render_cropped_img(lenses, lens_imgs, x1, y1, x2, y2):
+
+    """
+    Parameters
+    ----------
+
+    lenses: dictionary, keys are integer pairs (axial hex coordinates)
+            The lens dictionary
+
+    lens_imgs: dictionary
+               Dictionary with the lens data, same size as lenses
+
+    img_shape: pair of integers
+               Shape of the target image
+
+    Returns
+    -------
+
+    img:    array like
+            Two-dimensional array containing the microlens depth image
+            
+    """
+
+    assert len(lenses) == len(lens_imgs), "Number of lenses do not coincide"
+    assert len(lenses) > 0, "0 lenses supplied"
+    
+    first_lens = lenses[0, 0]
+    central_img = lens_imgs[0,0]
+
+    # ensure that the center lens is at the image origin
+    if img_shape is None:
+        img_shape = ((first_lens.pcoord) * 2 + 1).astype(int)
+
+    # check if it's gray image (disparity) or colored image
+    if len(central_img.shape) == 3:
+        hl, wl, c = central_img.shape
+    else:
+        hl, wl = central_img.shape
+        c = 1
+
+    # here we create the structure for the image (circle images with the mask)
+    assert hl == wl
+    n = (hl - 1) / 2.0
+    x = np.linspace(-n, n, hl)
+
+    XX, YY = np.meshgrid(x, x)
+    ind = np.where(XX**2 + YY**2 < first_lens.inner_radius**2)
+    
+    # micro image, so it takes the shape of first_lens.col_img_shape
+    if len(central_img.shape) == 3:
+        img = np.zeros((img_shape[0], img_shape[1], c))
+    else:
+        img = np.zeros((img_shape))
+    
+    for key in lenses:
+        
+        #pdb.set_trace()
+        data = np.asarray(lens_imgs[key])
+        #l_type = ((-key[0] % 3) +key[1]) % 3
+
+        lens = lenses[key]
+        ty = (YY + lens.pcoord[0] + 0.5).astype(int)
+        tx = (XX + lens.pcoord[1] + 0.5).astype(int)
+        
+        # ensure that the subimg is located within the image bounds
+        if np.any(ty < 0) or np.any(tx < 0) or np.any(ty >= img_shape[0]) or np.any(tx >= img_shape[1]):
+            continue
+
+        if len(data.shape) > 0:
+            img[(ty[ind], tx[ind])] = data[ind]
+        else:
+            img[(ty[ind], tx[ind])] = data
+    
+
+    
+    return img
+        
+   
