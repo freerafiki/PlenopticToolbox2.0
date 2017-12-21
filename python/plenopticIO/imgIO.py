125d4d65d9e89c429c60ba16ce381211a52469b9
PlenopticToolbox <34269091+PlenopticToolbox@users.noreply.github.com>
2017-12-21 17:07:49 +0100

    Add files via upload

---
+++ python/plenopticIO/imgIO.py
@@ -0,0 +1,466 @@
+"""
+File taking care of managing the images, reading and saving
+It works with both real (with .xml calibration file) and synthetic images (with scene.json file)
+The structure used to store micro-lens images is a python dictionary with lens element
+----
+@veresion v1 - December 2017
+@author Luca Palmieri
+"""
+
+import numpy as np
+import scipy.interpolate as sinterp
+import matplotlib.pyplot as plt
+import plenopticIO.lens_grid as rtxhexgrid
+import microlens.lens as rtxlens
+from xml.etree import ElementTree
+
+def _float_from_element(parent, element):
+
+    return float(parent.find(element).text)
+         
+def _floats_from_section(parent, section, elements):
+
+    sec = parent.findall(section)
+    
+    vals = dict()
+
+    if len(sec) != 1:
+        raise ValueError("Number of {0} entries != 1".format(section))
+
+    sec = sec[0]
+ 
+    for e in elements:
+        res = sec.findall(e)
+        if len(res) != 1:
+            raise ValueError("Number of {0} entries != 1".format(e))
+        vals[e] = float(res[0].text)
+
+    return vals
+
+def _section_with_attrib(parent, section, attr, val):
+    
+    elements = parent.findall(section)
+
+    result = None
+
+    for e in elements:
+        if e.attrib[attr] == val:
+            result = e
+            break
+
+    return result
+
+def read_calibration(filename):
+
+    tree = ElementTree.parse(filename)
+    root = tree.getroot()
+
+    calibration = dict()
+
+    calibration["offset"] = _floats_from_section(root, "offset", ["x", "y"])
+    calibration["diameter"] = _float_from_element(root, "diameter")
+    calibration["rotation"] = _float_from_element(root, "rotation")
+    calibration["lens_border"] = _float_from_element(root, "lens_border")
+    calibration["lens_base_x"] = _floats_from_section(root, "lens_base_x", ["x", "y"])
+    calibration["lens_base_y"] = _floats_from_section(root, "lens_base_y", ["x", "y"])
+    calibration["sub_grid_base"] = _floats_from_section(root, "sub_grid_base", ["x", "y"])
+    
+    lens_types = []
+    i = 0
+    sec = _section_with_attrib(root, "lens_type", "id", str(i))
+    
+    while sec is not None:
+        sub_dict = dict()
+        sub_dict["offset"] = _floats_from_section(sec, "offset", ["x", "y"])
+        sub_dict["depth_range"] = _floats_from_section(sec, "depth_range", ["min", "max"])
+        lens_types.append(sub_dict)
+        i += 1
+        sec = _section_with_attrib(root, "lens_type", "id", str(i))
+
+    calibration["lens_types"] = lens_types
+
+    return MLACalibration(calibration)
+
+class MLACalibration(object):
+
+    def __init__(self, calib):
+        
+        # the diameter of a single lens in pixels
+        self.lens_diameter = calib['diameter']
+
+        # lens radius in pixels
+        self.lens_radius = self.lens_diameter / 2.0
+
+        # pixel coordinates of the center lens
+        # raw image origin (0, 0) is upper left corner
+        # lower right corner is (h, w)
+        x, y = calib['offset']['x'], calib['offset']['y']
+        offset = (-y, x)
+        self.offset = np.array(offset)
+
+        # rotation angle of the grid, counter-clockwise
+        self.rot_angle = calib['rotation']
+
+        # lens border in pixels
+        self.lens_border = calib['lens_border']
+  
+        # lens bases in lens units
+        x, y = calib['lens_base_x']['x'], calib['lens_base_x']['y']
+
+        v = (x, y)
+        # reflect
+        lens_base_x = np.array([-v[1], v[0]])
+            
+        # lens bases in lens units
+        x, y = calib['lens_base_y']['x'], calib['lens_base_y']['y']
+        v = (x, y)
+        lens_base_y = np.array([-v[1], v[0]])
+
+        lbx = lens_base_x
+        lby = -lens_base_y + lens_base_x
+
+        # axial grid basis vectors in lens units
+        self.lbasis = np.vstack((lby, lbx)).T
+
+        # axial grid basis vectors in pixels
+        self.pbasis = self.lbasis * self.lens_diameter
+
+        self.inner_lens_radius = self.lens_radius - self.lens_border
+
+        # information about the focus range for eacht lens type
+        self.lens_types = calib['lens_types']
+
+
+def _hex_focal_type(c):
+    
+    """
+    Calculates the focal type for the three lens hexagonal grid
+    """
+
+    focal_type = ((-c[0] % 3) + c[1]) % 3
+
+    return focal_type 
+
+def load_from_xml(image_filename, config_filename):
+
+    img = plt.imread(image_filename)
+    calib = read_calibration(config_filename)
+    
+    if len(img.shape) < 3:
+        print("This version works only for colored image")
+        raise ValueError("Unsopported image dimension {0}".format(img.shape))    
+    elif len(img.shape) == 3:
+        weights = np.array([0.3, 0.59, 0.11])
+        data = np.sum([img[:, :, i] * weights[i] for i in range(3)], axis=0)
+        data_col = img
+    else:
+        raise ValueError("Unsupported image dimensions {0}".format(img.shape))
+        
+    # the image grid in pixels used for the bivariate spline interpolation
+    gridy, gridx = range(data.shape[0]), range(data.shape[1])
+
+    data_interp = sinterp.RectBivariateSpline(gridy, gridx, data)
+    data_interp_r = sinterp.RectBivariateSpline(gridy, gridx, data_col[:,:,0])
+    data_interp_g = sinterp.RectBivariateSpline(gridy, gridx, data_col[:,:,1])
+    data_interp_b = sinterp.RectBivariateSpline(gridy, gridx, data_col[:,:,2])
+    img_shape = np.asarray(data.shape[0:2])
+    coords = rtxhexgrid.hex_lens_grid(img_shape, calib.lens_diameter, calib.rot_angle, calib.offset, calib.lbasis)
+    focal_type_offsets = [tuple(lens['offset'].values()) for lens in calib.lens_types]
+    focal_type_offsets = [(int(p[0]), int(p[1])) for p in focal_type_offsets]
+    focal_type_map = {_hex_focal_type(p) : i for i, p in enumerate(focal_type_offsets)}
+    
+    lenses = dict()
+    local_grid = rtxlens.LocalLensGrid(calib.lens_diameter)
+    x, y = local_grid.x, local_grid.y
+    xx, yy = local_grid.xx, local_grid.yy
+    mask = np.zeros_like(local_grid.xx)
+    mask[xx**2 + yy**2 < calib.inner_lens_radius**2] = 1    
+    
+    for lc in coords:
+
+        pc = coords[lc]
+        lens = rtxlens.Lens(lcenter=lc, pcenter=pc, diameter=calib.lens_diameter)
+        lens.focal_type = _hex_focal_type(lc)
+        lens.fstop = lens.diameter
+        lens.pixel_size = 1.0
+        lens.position = np.array([pc[1], pc[0], 0])
+        lens.img = np.ones((local_grid.shape[0], local_grid.shape[1], 3))
+
+        cen_x = round(pc[0])
+        cen_y = round(pc[1])
+        x1 = int(cen_x + round(np.min(x)))
+        x2 = int(cen_x + round(np.max(x)))
+        y1 = int(cen_y + round(np.min(y)))
+        y2 = int(cen_y + round(np.max(y)))
+        lens.num_channels=3
+        lens.img = data_interp(y + pc[0], x + pc[1])
+        lens.img_interp = sinterp.RectBivariateSpline(y, x, lens.img)
+        # colored version of the interpolated image:
+        lens.img_interp3c[0] = data_interp_r
+        lens.img_interp3c[1] = data_interp_g
+        lens.img_interp3c[2] = data_interp_b
+        lens.col_img_uint = data_col[x1:x2+1, y1:y2+1] #np.zeros((lens.img.shape[0], lens.img.shape[1], lens.num_channels))
+
+        if lens.col_img_uint.dtype == 'uint8':
+            new_col_img = np.zeros((lens.col_img_uint.shape))
+            new_col_img = lens.col_img_uint / 255.0
+            lens.col_img = new_col_img
+        else:
+            lens.col_img = lens.col_img_uint
+
+        lens.mask = mask
+        lens.grid = local_grid
+        
+        # the lens area used for matching
+        lens.inner_radius = calib.inner_lens_radius
+
+        # minimum and maximum disparities for this lens
+        focal_type = focal_type_map[_hex_focal_type(lc)]
+        min_depth = calib.lens_types[focal_type]['depth_range']['min']
+        max_depth = calib.lens_types[focal_type]['depth_range']['max']
+
+        lens.min_disp = lens.diameter / max_depth
+        lens.max_disp = lens.diameter / min_depth
+        #pdb.set_trace()
+
+        lenses[tuple(lc)] = lens
+
+    return lenses    
+    
+def load_from_json(filename):
+
+    """
+    Loads a synthetic MLA dataset
+
+    Parameters:
+    -----------
+
+    filename: string
+              Filename of the scene configuration file (format: JSON)
+
+    Returns:
+    --------
+
+    lenses: dictionary
+              Lens dictionary, keys are the axial coordinates
+    """
+    
+    basedir = os.path.dirname(filename)
+
+    with open(filename, 'r') as f:
+        lenses_json = json.load(f)
+
+    lenses = dict()
+
+    if len(lenses_json) == 0:
+        return lenses
+
+    diam = lenses_json[0]['diameter']
+    radius = diam / 2.0
+    inner_radius = radius - lenses_json[0]['lens_border']
+    local_grid = rtxlens.LocalLensGrid(diam)
+  
+    # local grid coordinates shared by all lenses
+    x, y = local_grid.x, local_grid.y
+    xx, yy = local_grid.xx, local_grid.yy
+    mask = np.zeros_like(local_grid.xx)
+    mask[xx**2 + yy**2 < inner_radius**2] = 1
+    
+    for lens in lenses_json:
+        # image data
+        color_filename = "{0}/{1}".format(basedir, lens['relative_color_filename'])
+
+        # ground truth depth
+        depth_filename = "{0}/{1}".format(basedir, lens['relative_depth_filename'])
+        
+        tmp_lens = rtxlens.Lens(lens['axial_coord'], lens['pixel_coord'], lens['diameter'], lens['focal_type'])
+        
+        tmp_lens.col_img = plt.imread(color_filename)
+        if tmp_lens.col_img.shape[2] == 4:
+          tmp_lens.col_img = tmp_lens.col_img[:,:,:3]
+        weights = np.array([0.3, 0.59, 0.11])
+        tmp_lens.img = np.sum([tmp_lens.col_img[:, :, i].astype(np.float64) * weights[i] for i in range(3)], axis=0)
+        tmp_lens.grid = local_grid
+        tmp_lens.mask = mask
+        tmp_lens.num_channels = tmp_lens.col_img.shape[2]
+        tmp_lens.img_interp = sinterp.RectBivariateSpline(y, x, tmp_lens.img)
+        tmp_lens.img_interp3c[0] = sinterp.RectBivariateSpline(y, x, tmp_lens.col_img[:,:,0])
+        tmp_lens.img_interp3c[1] = sinterp.RectBivariateSpline(y, x, tmp_lens.col_img[:,:,1])
+        tmp_lens.img_interp3c[2] = sinterp.RectBivariateSpline(y, x, tmp_lens.col_img[:,:,2])
+        tmp_lens.inner_radius = inner_radius
+
+        tmp_lens.focal_length = lens['focal_length']
+        tmp_lens.focus_distance = lens['focus_distance']
+        tmp_lens.fstop = lens['fstop']
+        tmp_lens.pixel_size = lens['pixel_size']
+        tmp_lens.position = np.array(lens['position'])  
+        #tmp_lens.position = np.array(lens['location'])  # some datasets needs 'location'
+        tmp_lens.rotation = np.array(lens['rotation_mat']).reshape((3, 3)) 
+        #tmp_lens.rotation = np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]).reshape((3,3)) #
+        
+        tmp_lens.focus_disparity = tmp_lens.focal_length * tmp_lens.diameter / tmp_lens.focus_distance
+        
+        # read the ground truth depth. The 16-Bit PNG depth files
+        # contain the clipped depth values, convert to the real depth
+        # values        
+        tmp_depth = plt.imread(depth_filename).astype(np.float64) 
+        tmp_depth = tmp_depth * (lens['clip_end'] - lens['clip_start']) + lens['clip_start']
+        tmp_lens.depth_img = tmp_depth
+        tmp_lens.disp_img = np.copy(tmp_depth)
+        tmp_lens.disp_img[tmp_depth > 0] = (tmp_lens.focal_length * tmp_lens.diameter) / tmp_lens.disp_img[tmp_depth > 0]
+
+        # set the lens border
+        tmp_lens.lens_border = lens['lens_border']
+
+        # finally add the lens to the dictionary
+        lenses[tuple(lens['axial_coord'])] = tmp_lens
+
+    return lenses    
+    
+def save_xml(filename, lenses):
+
+    """
+    Saves a synthetic dataset in the Raytrix Format: PNG Image file + xml config
+    
+    Attention: The PNG File is saved as a 4 channel PNG
+
+    Parameters
+    ----------
+
+    filename: string
+      The filename for the target files. If filename
+      is <path>/file the target files are <path>/file.png, <path>/file.xml
+
+    lenses: dictionary
+      Dictionary of lenses, keys are axial coordinates
+      
+    """
+
+    # extract the images as a seperate dictionary for the rendering process
+    lens_data = dict()
+    for lcoord in lenses:
+        lens_data[lcoord] = lenses[lcoord].img
+
+    # render the MLA image
+    img = rtxrender.render_lens_imgs(lenses, lens_data)
+
+
+    h, w = img.shape
+    m = ((h-1)/2.0, (w-1) / 2.0)
+    offset = lenses[0, 0].pcoord - np.array(m)
+
+    l = lenses[0, 0]
+    config_template = _xml_template()
+    config = config_template.substitute(offset_y=offset[0], offset_x=offset[1], diam=l.diameter,
+                                        angle=0, lens_border=1.0)
+
+    img_filename = "{0}.png".format(filename)
+    config_filename = "{0}.xml".format(filename)
+
+    with open(config_filename, "w") as f:
+        f.write(config)
+
+    plt.imsave(img_filename, img, cmap='gray')
+
+    return config
+    
+def _xml_template():
+    s = string.Template('''<RayCalibData version="1.0">
+  <offset units="pix">
+    <x>$offset_x</x>
+    <y>$offset_y</y>
+  </offset>
+  <diameter units="pix">$diam</diameter>
+  <rotation units="rad">$angle</rotation>
+  <lens_border units="pix">$lens_border</lens_border>
+  <tcp units="virtual_depth">2.000000000000</tcp>
+  <lens_base_x units="lens">
+    <x>1.000000000000</x>
+    <y>0.000000000000</y>
+  </lens_base_x>
+  <lens_base_y units="lens">
+    <x>0.500000000000</x>
+    <y>0.866025403784</y>
+  </lens_base_y>
+  <sub_grid_base units="lens">
+    <x>3.000000000000</x>
+    <y>1.732050807569</y>
+  </sub_grid_base>
+  <distortion type="radial">
+    <function>constant</function>
+    <parameter count="0">0</parameter>
+    <offset units="pix">
+      <x>0.000000000000</x>
+      <y>0.000000000000</y>
+    </offset>
+  </distortion>
+  <lens_type id="0">
+    <offset units="lens">
+      <x>0.000000000000</x>
+      <y>0.000000000000</y>
+    </offset>
+    <depth_range units="virtual_depth">
+      <min>1.000000000000</min>
+      <max>3.000000000000</max>
+    </depth_range>
+  </lens_type>
+  <lens_type id="1">
+    <offset units="lens">
+      <x>1.000000000000</x>
+      <y>0.000000000000</y>
+    </offset>
+    <depth_range units="virtual_depth">
+      <min>2.799999952316</min>
+      <max>4.000000000000</max>
+    </depth_range>
+  </lens_type>
+  <lens_type id="2">
+    <offset units="lens">
+      <x>-1.000000000000</x>
+      <y>0.000000000000</y>
+    </offset>
+    <depth_range units="virtual_depth">
+      <min>3.799999952316</min>
+      <max>100.000000000000</max>
+    </depth_range>
+  </lens_type>
+</RayCalibData>
+''')
+    return s
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
---
+++ python/plenopticIO/lens_grid.py
@@ -0,0 +1,237 @@
+"""
+It contains the information about the hexagonal grid of the micro-lenses
+----
+@veresion v1 - December 2017
+@author Luca Palmieri
+"""
+
+import numpy as np
+
+# coefficients of the basis vectors (pby, pbx) in lens units for the
+# first three rings of the same lens type
+
+# type 1: current
+# type 2: right neighbour
+# type 3: left neighbour
+
+HEX_TYPE_OFFSETS = [
+                    [
+                     [[ 1,  1], [ 2, -1], [ 1, -2],
+                      [-1, -1], [-2,  1], [-1,  2]],
+                     [[0,  3], [ 3, 0], [ 3, -3], 
+                      [0, -3], [-3, 0], [-3,  3]]
+                    ],
+                    [
+                     [[0, 1], [1, -1], [-1, 0]],
+                     [[2, 0], [0, -2], [-2, 2]],
+                     [[ 1,  2], [ 3, -2], [ 2, -3],
+                      [-2, -1], [-3,  1], [-1,  3]]
+                    ],
+                    [
+                     [[1, 0], [0, -1], [-1, 1]],
+                     [[0, 2], [2, -2], [-2, 0]],
+                     [[ 2,  1], [ 3, -1], [ 1, -3],
+                      [-1, -2], [-3,  2], [-2,  3]]
+                    ]
+                  ]
+
+# neighbours of all types, sorted by distance in lens units
+# ring[0]: 1 lens
+# ring[1]: 1 * sqrt*(3) lenses
+# ring[2]: 2 lenses
+# ring[3]: ...
+
+HEX_OFFSETS = [
+                 [
+                  [ 0,  1], [ 1,  0], [ 1, -1],
+                  [ 0, -1], [-1,  0], [-1,  1]
+                 ],
+                 [
+                  [ 1,  1], [ 2, -1], [ 1, -2],
+                  [-1, -1], [-2,  1], [-1,  2]
+                 ],
+                 [
+                  [ 0,  2], [ 2,  0], [ 2, -2], 
+                  [ 0, -2], [-2,  0], [-2,  2]
+                 ],
+                 [
+                  [ 1,  2], [ 2,  1], [ 3, -1], [ 3, -2],
+                  [ 2, -3], [ 1, -3], [-1, -2], [-2, -1],
+                  [-3,  1], [-3,  2], [-2,  3], [-1,  3]
+                 ],
+                 [
+                  [0,  3], [ 3, 0], [ 3, -3],
+                  [0, -3], [-3, 0], [-3,  3]
+                 ],
+                 [
+                  [ 2,  2], [ 4, -2], [ 2, -4],
+                  [-2, -2], [-4,  2], [-2,  4]
+                 ],
+                 [
+                  [ 1,  3], [ 3,  1],  [ 4, -1], [ 4, -3],
+                  [ 3, -4], [ 1, -4],  [-1, -3], [-3, -1],
+                  [-4,  1], [-4,  3],  [-3,  4], [-1,  4]
+                 ],
+                 [
+                  [0,  4], [ 4, 0], [ 4, -4],
+                  [0, -4], [-4, 0], [-4,  4]
+                 ]
+               ]
+
+HEX_DIRECTIONS = HEX_OFFSETS[0]
+
+SCAM_OFFSETS = [[2,-2], [1,-1], [2,-1], [1,0], [2,0], [0,-2], [0,-1], [0,1], [0,2], [-2,0], [-1,0], [-2,1],[-1,1],[-2,2]]
+
+class HexDir(object):
+    E =  HEX_DIRECTIONS[0]
+    SE = HEX_DIRECTIONS[1]
+    SW = HEX_DIRECTIONS[2]
+    W =  HEX_DIRECTIONS[3]
+    NW = HEX_DIRECTIONS[4]
+    NE = HEX_DIRECTIONS[5]
+    
+def hex_focal_type(c):
+    
+    """
+    Calculates the focal type for the three lens hexagonal grid
+    """
+
+    focal_type = ((-c[0] % 3) + c[1]) % 3
+
+    return focal_type 
+    
+
+def hex_lens_grid(img_shape, diam, angle, offset, B, filter_method='lens'):
+    
+    """
+    Parameters:
+    
+    h: integer
+       image height in pixels
+    w: integer
+        image width in pixels
+    diam: float
+        lens diameter in pixels
+    angle: float
+        rotation angle of the grid, counter-clockwise in radians
+    offset: array-like
+        (y, x) offset in pixels of the center lens
+    B: array-like
+        2x2 matrix, basis vectors of the hex grid, each colum represents a basis vector
+    
+    filter_method: string
+        'lens': removes all lenses which are not completely inside the image region
+        'center': removes all lenses whose centers are not within the image region
+        
+    Returns: centers, array-like
+             array of (y, x) lens center coordinates in pixels
+             
+    Convention: Origin of the image: (0, 0) upper left corner, positive y axis points down
+    """
+
+    B = np.asarray(B)
+   
+
+    if np.linalg.norm(B[0]) > 1:
+        switch_xy = True
+    else:
+        switch_xy = False
+
+    print("Basis: {0} switch {1}".format(B, switch_xy))
+    
+    lens_centers = []
+    h, w = img_shape
+    img_center = np.array([(h-1)/2.0, (w-1)/2.0])
+    
+    # conservative estimate of lenses in y and x direction to cover the image
+    ny = int(np.ceil(h * np.sqrt(2) / diam) + 2)
+    nx = int(np.ceil(w * np.sqrt(2) / diam) + 4)
+    
+    r = diam / 2.0
+    
+    # start offsets, upper left corner
+    sx = - (nx * diam - w) / 2.0
+    sy = - (ny * r * np.sqrt(3) - h) / 2.0
+    
+    # generate the lens center coordinates in pixels
+    for i in range(ny):
+        py = i * np.sqrt(3) * r + sy
+        for j in range(nx):
+            px = (sx + j * diam) + (i % 2) * r
+            
+            if switch_xy is True:
+                lens_centers.append(np.array([px, py]))
+            else:
+                lens_centers.append(np.array([py, px]))
+
+    # lens center closest to the image center
+    lorigin = _lens_origin(lens_centers, img_center)
+
+    # adjust the grid: lorigin is the new origin
+    lens_centers = [c - lorigin for c in lens_centers]
+    
+    lenses = _axial_coordinates(B * diam, lens_centers)
+    tlenses = _transform_grid(lenses, img_center, angle, offset)
+    
+    filters = dict()
+    filters['center'] = lambda p: p[0] >=0 and p[0] < h and p[1] >= 0 and p[1] < w
+    filters['lens']   = lambda p: (p[0] - r) >=0 and (p[0] + r) < h and  \
+                                  (p[1] - r) >= 0 and (p[1] + r) < w
+    
+    if filter_method in filters:
+        
+        tlenses = {key: tlenses[key] for key in tlenses 
+                   if filters[filter_method](tlenses[key])}
+        
+    return tlenses
+   
+def _lens_origin(lens_centers, img_center):
+    
+    
+    """
+    Parameters:
+    
+    h: integer, image height in pixels
+    w: integer, image width in pixels
+    centers: array of lens center coordinates (y, x) in pixels
+    
+    Returns: 
+    
+    centers: array-like, lens center closest to the image center
+    """
+    
+    # distance from the lens centers to the image center
+    dist = [np.linalg.norm(c - img_center) for c in lens_centers]
+    return lens_centers[np.argmin(dist)]
+
+def _axial_coordinates(B, centers):
+    
+    """
+    Parameters:
+    
+    B: 2x2 matrix, grid basis vectors
+    
+    Returns: 
+    
+    axial_coords: dictionary, keys: axial coordinates, 
+                  values:center coordinates in pixels
+    """
+    
+    lenses = dict()
+
+    for c in centers:
+        tmp = np.linalg.solve(B, c)
+        axial_coord = tuple(np.round(tmp).astype(int))
+        lenses[axial_coord] = c
+
+    return lenses
+
+def _transform_grid(lenses, img_center, angle, offset):
+    
+    # rotation matrix, counter-clockwise
+    R = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    
+    acenters = {key: np.dot(R, lenses[key]) + offset + img_center
+                for key in lenses}
+        
+    return acenters
